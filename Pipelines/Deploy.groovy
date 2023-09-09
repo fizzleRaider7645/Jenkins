ebdfc5805602bae73b6008102919b11f8a917f40
Douglas Uretsky <douglasuretsky@gmail.com>
2023-09-09 13:57:06 -0400

    f

---
+++ Pipelines/Deploy.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent {
+        label any
+    }
+    environment {
+        NPM_CONFIG_CACHE = "${WORKSPACE}/.npm"
+        AWS_ACCESS_KEY_ID = credentials('my-aws-access-key-id')
+        AWS_SECRET_ACCESS_KEY = credentials('my-aws-secret-access-key')
+    }
+    stages {
+        stage('Build/Deploy') {
+            steps {
+                script {
+                    docker.image('node:lts').inside('-u 0:0') {
+                        sh 'apt-get update && apt-get install -y awscli'
+                        sh 'yarn --frozen-lockfile'
+                        sh 'node --version'
+                        sh 'ls -la'
+                        sh 'yarn run build'
+                        sh 'echo ${WORKSPACE}'
+                        sh 'aws s3 cp ${WORKSPACE}/build s3://avocado-blue/ --recursive'
+                    }
+                }
+            }
+        }
+    }
+}
--- Pipelines/PR.groovy
+++ Pipelines/PR.groovy
@@ -1,25 +1,18 @@
 pipeline {
     agent {
-        label 'build-agent-2'
+        label any
     }
     environment {
         NPM_CONFIG_CACHE = "${WORKSPACE}/.npm"
-        AWS_ACCESS_KEY_ID = credentials('my-aws-access-key-id')
-        AWS_SECRET_ACCESS_KEY = credentials('my-aws-secret-access-key')
     }
     stages {
-        stage('Build/Deploy') {
+        stage('Build') {
             steps {
                 script {
-                    docker.image('node:lts').inside('-u 0:0') {
-                        sh 'apt-get update && apt-get install -y awscli'
-                        sh 'yarn --frozen-lockfile'
-                        sh 'node --version'
-                        sh 'ls -la'
-                        sh 'yarn run build'
-                        sh 'echo ${WORKSPACE}'
-                        sh 'aws s3 cp ${WORKSPACE}/build s3://avocado-blue/ --recursive'
-                    }
+                    sh 'yarn --frozen-lockfile'
+                    sh 'node --version'
+                    sh 'ls -la'
+                    sh 'yarn run build'
                 }
             }
         }
