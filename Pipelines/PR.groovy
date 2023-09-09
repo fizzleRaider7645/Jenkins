6b19261e90bb9b90a6ec4038d7aecac451f9a9d4
Douglas Uretsky <douglasuretsky@gmail.com>
2023-09-09 15:12:16 -0400

    f

--- Pipelines/PR.groovy
+++ Pipelines/PR.groovy
@@ -6,14 +6,15 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                docker.image('node:lts').inside('-u 0:0') {
-                    sh '''
-                        yarn --frozen-lockfile
-                        node --version
-                        ls -la
-                        yarn run build
-                        ls -la ${WORKSPACE}/build
-                    '''
+                script {
+                    docker.image('node:lts').inside('-u 0:0') {
+                        sh '''
+                          yarn --frozen-lockfile
+                          node --version
+                          ls -la
+                          yarn run build
+                        '''
+                    }
                 }
             }
         }
