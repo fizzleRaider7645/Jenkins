635f8a3cb1cd7cf08fcfb026e09b4bb3ed002bc6
Douglas Uretsky <douglasuretsky@gmail.com>
2023-09-09 13:34:48 -0400

    f

--- Pipelines/PR.groovy
+++ Pipelines/PR.groovy
@@ -4,12 +4,15 @@ pipeline {
     }
     environment {
         NPM_CONFIG_CACHE = "${WORKSPACE}/.npm"
+        AWS_ACCESS_KEY_ID = credentials('my-aws-access-key-id')
+        AWS_SECRET_ACCESS_KEY = credentials('my-aws-secret-access-key')
     }
     stages {
         stage('Build') {
             steps {
                 script {
                     docker.image('node:lts').inside {
+                        sh 'apt-get update && apt-get install -y awscli'
                         sh 'yarn --frozen-lockfile'
                         sh 'node --version'
                         sh 'ls -la'
